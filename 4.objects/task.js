e4e49031b2f2f3981f9982c813fc42b23641e7b3
Elvirin9 <86201674+Elvirin9@users.noreply.github.com>
2023-04-05 18:42:52 +0300

    Update task.js

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,30 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMark = function (mark){
+  if (this.marks === undefined) this.marks = []; 
+  this.marks.push(mark);
 }
 
-Student.prototype.getAverage = function () {
-  
+Student.prototype.addMarks = function (...mark){
+  if (this.marks === undefined) this.marks = [];
+  for (element of mark) this.marks.push(element);
 }
 
-Student.prototype.exclude = function (reason) {
-  
+Student.prototype.getAverage = function (){
+  let reducer = (previousValue, currentValue) => previousValue + currentValue;
+  return (this.marks.reduce(reducer) / this.marks.length);
+}
+
+Student.prototype.exclude = function (reason){
+  delete this.marks;
+  delete this.subject;
+  this.excluded = reason;
 }
