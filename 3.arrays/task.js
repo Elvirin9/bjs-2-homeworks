fad4baf6ed7d31ab09a0d26965433b43c795dcb9
Elvirin9 <86201674+Elvirin9@users.noreply.github.com>
2023-04-05 18:32:13 +0300

    Update task.js

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,11 +1,10 @@
 function compareArrays(arr1, arr2) {
-  let result = (arr1.length === arr2.length && arr1.every((item, index) => item === arr2[index]));
-  return result;
-}
-
-function advancedFilter(arr) {
-  let resultArr = arr.filter((positive) => positive > 0)
-    .filter ((multipe) => multipe % 3 === 0)
-    .map ((multiplied) => multiplied * 10)
-  return resultArr;
-}
+    if (arr1.length === arr2.length){
+      return arr1.every((element, index) => element === arr2[index]);
+    } else return false;
+  }
+  
+  function getUsersNamesInAgeRange(users, gender) {
+    let result = users.filter( user => user.gender === gender).reduce((accumulator, current, index, array) => accumulator + current.age / array.length, 0);
+    return result;
+  }
